ca823fdfb99eb8eb16ad103705fd6fab352da42e
Claudia Hill <hillc255@yahoo.com>
2023-06-13 13:04:21 +0000

    creating custom trigger, modals, refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
+import React, { useEffect, useState } from 'react';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
   const name = 'StarGazers'
+  const [cast, setCast] = useState([]);
+  let [memberInfo, setMemberInfo] = useState(null);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+  useEffect(() => {
+    fetchCast();
+  });
+
   return (
     <div className="container">
-      <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
+          <h1>Meet the Stargazers</h1>
+          <p>Members of an <b>intergalactic alliance</b>
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast />
-          <Support />
+          <ListCast cast={cast} onChoice={(info) => {setMemberInfo(info)}} />
+          {memberInfo && <Modals member={memberInfo} handleClose={()=>{setMemberInfo(null)}} />}
         </hgroup>
-      </article>
     </div>
   )
 }
